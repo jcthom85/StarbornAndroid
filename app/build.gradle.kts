b08ab4003d277bb347fb09afe9c9407b7ff8e745
Jason Thomas <jcthom85@github.com>
2026-06-26 16:39:40 -0400

    Bump release version to 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.junewiregames.starborn.prealpha"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.1.6"
+        versionCode = 9
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
