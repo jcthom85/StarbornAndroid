5df1eb1cfd09c03785ed4fe761eb3a77e751055c
Jason Thomas <jcthomas@vims.edu>
2026-08-29 12:39:35 -0400

    Bump version to v1.1.49 (versionCode 51)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.junewiregames.starborn.prealpha"
         minSdk = 26
         targetSdk = 35
-        versionCode = 50
-        versionName = "1.1.48"
+        versionCode = 51
+        versionName = "1.1.49"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
